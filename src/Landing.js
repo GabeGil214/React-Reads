cd022266081109e73e4ccac54363c289f005e757
Gabe <gilgabe214@gmail.com>
2019-01-15 22:02:43 -0500

    refactor: add landing page

---
+++ src/Landing.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import './landing.css'
+
+
+ class Landing extends Component {
+  render(){
+    return (
+      <Route exact path='/'  render = {() => (
+          <div className="hero-image-container">
+            <div className="hero-layer">
+              <div className="hero-text-container">
+                <h2>Welcome to My Reads!</h2>
+                <div className="link-container">
+                  <Link to="/search"><button>Search for Books</button></Link>
+                  <Link to="/library"><button>My Library</button></Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    )
+  }
+}
+ export default Landing
